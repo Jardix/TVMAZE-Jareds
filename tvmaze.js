a1586741a5d66af69ef0d81c8b24609af78eb6b7
jardix <jaridox@hotmail.com>
2021-07-23 13:16:11 -0700

    Done

--- tvmaze.js
+++ tvmaze.js
@@ -18,21 +18,14 @@
         image: <an image from the show data, or a default imege if no image exists, (image isn't needed until later)>
       }
  */
+
+const MISSING_IMAGE_URL = "https://tinyurl.com/missing-tv"
+
 async function searchShows(query) {
   // TODO: Make an ajax request to the searchShows api.  Remove
   // hard coded data.
 
-  // return [
-  //   {
-  //     id: 1767,
-  //     name: "The Bletchley Circle",
-  //     summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-  //     image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-  //   }
-  // ]
-
-
-  const response = await axios.get(`http://api.tvmaze.com/search/showsq=${query}`);
+  let response = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
 
   let shows = response.data.map(result => {
     let show = result.show;
@@ -40,7 +33,7 @@ async function searchShows(query) {
       id: show.id,
       name: show.name,
       summary: show.summary,
-      image: show.image
+      image: show.image ? show.image.medium : MISSING_IMAGE_URL,
     }
   });
   return shows;
@@ -60,9 +53,11 @@ function populateShows(shows) {
     let $item = $(
       `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
          <div class="card" data-show-id="${show.id}">
+         <img class="card-img-top" src="${show.image}">
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
+             <button class="btn btn-primary get-episodes">Episodes</button>
            </div>
          </div>
        </div>
@@ -100,20 +95,43 @@ async function getEpisodes(id) {
   // TODO: get episodes from tvmaze
   //       you can get this by making GET request to
   //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
-  const response = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
+  let response = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
   // TODO: return array-of-episode-info, as described in docstring above
-  return [
-    {
-      id: response.id,
-      name: response.name,
-      summary: response.summary,
-      img: response.img      
-    }
-  ]
+  let episodes = response.data.map(episode => ({
+    id: episode.id,
+    name: episode.name,
+    season: episode.season,
+    number: episode.number
+  }))
+  return episodes;
 }
 
+// This is the 'extra' content I didn't even attempt, after spending as much time as I did on the 'main content'. 
+
 /** Populate episodes list:
- *     - given list of episodes, add espiodes to DOM
+ *     - given list of episodes, add episodes to DOM
  */
+function populateEpisodes(episodes) {
+  const $episodesList = $("#episodes-list");
+  $episodesList.empty();
+
+  for (let episode of episodes) {
+    let $item = $(
+      `<li>
+        ${episode.name}
+        (season ${episode.season}, episode ${episode.number})
+      </li>
+      `);
+
+    $episodesList.append($item);
+  }
+  $("#episodes-area").show();
+}
+
 
 /** Handle click on show name. */
+$('#shows-list').on("click", ".get-episodes", async function handleEpisodeClick(evt) {
+  let showId = $(evt.target).closest(".Show").data("show-id");
+  let episodes = await getEpisodes(showId);
+  populateEpisodes(episodes);
+});
